47870e6b6dd8b0e4b3b065d98f8824c702c540e8
Dan Retegan <danretegan@yahoo.com>
2024-06-17 13:33:36 +0300

    Setting up a Real-time Listener
    .onSnapshot()
    .docChanges()

--- src/scripts/chat.js
+++ src/scripts/chat.js
@@ -9,6 +9,8 @@ class Chatroom {
     this.username = username;
     this.chats = db.collection('chats');
   }
+
+  //*   metoda addChat (adauga un mesaj):
   async addChat(message) {
     //* format a chat object:
     const now = new Date();
@@ -23,15 +25,22 @@ class Chatroom {
     const response = await this.chats.add(chat);
     return response;
   }
+
+  //* metoda getChats (actualizeaza in timp real mesajele):
+  getChats(callback) {
+    this.chats.onSnapshot(snapshot => {
+      snapshot.docChanges().forEach(elem => {
+        if (elem.type === 'added') {
+          //* update the UI:
+          callback(elem.doc.data());
+        }
+      });
+    });
+  }
 }
 
 const chatroom = new Chatroom('gaming', 'shaun');
 
-chatroom
-  .addChat('hello everyone!')
-  .then(() => {
-    console.log('chat added!');
-  })
-  .catch(err => {
-    console.log(err);
-  });
+chatroom.getChats(data => {
+  console.log(data);
+});

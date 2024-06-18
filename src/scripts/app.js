7f109bf2b096e971e66473c8b2e032ec72002c38
Dan Retegan <danretegan@yahoo.com>
2024-06-18 12:28:08 +0300

    Changing Username

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,11 +4,14 @@ import { Chatroom } from './chat.js';
 //! DOM queries:
 const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const upadateMssg = document.querySelector('.update-mssg');
 
 //! add a new chat:
 newChatForm.addEventListener('submit', e => {
   e.preventDefault();
 
+  //! pentru ca id-ul sau numele campului input este message putem folosi metoda .message:
   const message = newChatForm.message.value.trim();
   chatroom
     .addChat(message)
@@ -18,6 +21,24 @@ newChatForm.addEventListener('submit', e => {
     .catch(err => {
       console.log(err);
     });
+});
+
+//! update username:
+newNameForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  //! update name via chatroom (pentru ca id-ul sau numele campului input este name putem folosi metoda .name):
+  const newName = newNameForm.name.value.trim();
+  chatroom.updateName(newName);
+
+  //! reset the form:
+  newNameForm.reset();
+
+  //! show and hide the update message:
+  upadateMssg.innerText = `Your name was updated to ${newName}`;
+  setTimeout(() => {
+    upadateMssg.innerText = '';
+  }, 3000);
 });
 
 //! class instances:
